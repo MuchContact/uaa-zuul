6b689774dd33fa7f80a0ea2ebfa92abb2ab3ab37
MuchContact <422549477@qq.com>
2019-03-17 20:16:44 +0800

    添加client_id注释

--- auth-server/src/main/java/com/hfcsbc/config/AuthorizationServerConfig.java
+++ auth-server/src/main/java/com/hfcsbc/config/AuthorizationServerConfig.java
@@ -1,34 +1,17 @@
 package com.hfcsbc.config;
 
-import com.hfcsbc.security.DomainUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import javax.jws.soap.SOAPBinding;
-import javax.sql.DataSource;
-import java.security.KeyPair;
 
 /**
  * Created by wangyunfei on 2017/6/9.
@@ -63,6 +46,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()")
                 .allowFormAuthenticationForClients();
+        //org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer#clientCredentialsTokenEndpointFilter
+        //允许通过client_id&client_secret调用
+        // curl -X POST \
+        //  http://localhost:9999/oauth/token \
+        //  -H 'Content-Type: application/x-www-form-urlencoded' \
+        //  -H 'Postman-Token: 3ca2c78b-66ac-4529-9b43-6302323c8acb' \
+        //  -H 'cache-control: no-cache' \
+        //  -d 'grant_type=password&username=admin&password=admin&client_id=android&client_secret=android&undefined='
     }
 
     @Override
